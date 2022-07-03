c7e02fa35436cec2889ddf4bb66a4a63aa06ee2e
Jeff <yuheng.toh@carro.co>
2022-07-03 16:55:52 +0800

    Mod data conversion script, mod EDA notebook to read new columns

--- data-retriever/convert_csv.py
+++ data-retriever/convert_csv.py
@@ -4,9 +4,10 @@ import pandas as pd
 with open('903.json', 'r') as f:
     for line in f.readlines():
         item = json.loads(line.strip('\n'))
-        df = pd.json_normalize(item['Services'])
+        df = pd.DataFrame(index=[0])
         df['BusStopCode'] = item['BusStopCode']
         df['NextStop'] = item['NextStop']
         df['CurrentTime'] = item['CurrentTime']
-        print(df)
+        df = pd.concat([df, pd.json_normalize(item['Services'])], axis=1)
+        df.to_csv('test.csv', mode='a', header=False, index=False)
         
