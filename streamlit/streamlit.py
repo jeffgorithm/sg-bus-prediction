4758974dedb3d16ef1bbb00e76288bd06553ff3f
Jeff <yuheng.toh@carro.co>
2022-07-03 18:06:28 +0800

    Manually retrieved coordinates data for service 903, added impl to read coordinates and display

--- streamlit/streamlit.py
+++ streamlit/streamlit.py
@@ -2,6 +2,35 @@ import streamlit as st
 import pandas as pd
 import json
 
+def read_route(file_path):
+    '''Reads route file and returns list of BusStopCode and ServiceNo'''
+    file = pd.read_csv(file_path, index_col=None)
+    bus_stops = []
+
+    for _, row in file.iterrows():
+        bus_stops.append(str(row['BusStopCode']))
+
+    return bus_stops
+
+def read_bus_stops(file_path):
+    bus_stop_dict = {}
+
+    lat, lon = [], []
+
+    with open(file_path, 'r') as f:
+        for line in f.readlines():
+            lines = line.strip('\n').split(',')
+            lat.append(float(lines[1]))
+            lon.append(float(lines[2]))
+            bus_stop_dict[lines[0]] = (float(lines[1]), float(lines[2]))
+
+    df = pd.DataFrame({
+        'lat' : lat,
+        'lon' : lon
+    })
+
+    return df
+
 def read_location():
     coordinates = set()
 
@@ -30,9 +59,11 @@ def read_location():
 
 if __name__ == '__main__':
     df = read_location()
+    bus_route = read_route('../data-retriever/routes/903_bus_stops.csv')
+    df = read_bus_stops('903_stops_coordinates.csv')
 
     st.title('Predicting Bus Travel Times')
 
-    print(df.head())
+    print(df.head(30))
 
     st.map(df)
